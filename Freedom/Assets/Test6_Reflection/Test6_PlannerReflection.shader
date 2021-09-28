719b8bd6915e3065e34407dab8cd2c7df1abc00e
IvanMaoCompany1 <mao17543@163.com>
2021-09-28 15:09:46 +0800

    Add Test6_Reflection

---
+++ Freedom/Assets/Test6_Reflection/Test6_PlannerReflection.shader
@@ -0,0 +1,102 @@
+﻿Shader "Reflection/PlanarReflection"
+{
+	Properties
+	{
+		_reflectionColorLerpA("ReflectionColorLerpA",Color) = (0,0,0,0)
+		_reflectionColorLerpB("ReflectionColorLerpB",Color) = (0,0,0,0)
+	}
+
+	SubShader
+	{
+		Tags{ "RenderType" = "Opaque" "LightMode" = "ForwardBase" }
+		//Tags{ "LightMode" = "ForwardBase" }
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#pragma multi_compile_fwdbase
+			#include "UnityCG.cginc"
+			#include "AutoLight.cginc"
+			#include "UnityLightingCommon.cginc"
+
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+				half3 normal : NORMAL;
+			};
+
+			struct v2f
+			{
+				float4 vertex : SV_POSITION;
+				half4 diffuse : COLOR0;
+				float4 screenPos : TEXCOORD0;
+				float2 uv : TEXCOORD1;
+				half4 ambient : TEXCOORD2;
+				SHADOW_COORDS(3)
+				//float4 _ShadowCoord : TEXCOORD2;
+			};
+
+			sampler2D _ReflectionTex;
+			float4 _reflectionColorLerpA;
+			float4 _reflectionColorLerpB;
+
+			v2f vert(appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				o.screenPos = ComputeScreenPos(o.vertex);
+				//o._ShadowCoord = ComputeScreenPos(o.vertex);
+
+				half3 wNormal = UnityObjectToWorldNormal(v.normal);
+				half nl = max(0, dot(wNormal, _WorldSpaceLightPos0.xyz));
+				o.diffuse = nl * _LightColor0;
+				o.ambient = half4(ShadeSH9(half4(wNormal, 1)), 0.0f);
+
+
+				TRANSFER_SHADOW(o);
+
+				return o;
+			}
+
+			fixed4 frag(v2f i) : SV_Target
+			{
+				fixed4 col = tex2D(_ReflectionTex, i.screenPos.xy / i.screenPos.w);
+				fixed4 finalColor = lerp(_reflectionColorLerpA, _reflectionColorLerpB, col);
+				half shadow = SHADOW_ATTENUATION(i);
+
+				half4 diffuse = i.diffuse * shadow + i.ambient;
+
+				//fixed4 shadowColor = lerp(float4(0.8, 0.8, 0.8, 1), float4(1, 1, 1, 1), step(0.5, SHADOW_ATTENUATION(i)));
+
+				return finalColor * diffuse;
+			}
+			ENDCG
+		}
+
+		UsePass "VertexLit/SHADOWCASTER"
+		//Pass
+		//{
+		//	Tags{ "LightMode" = "ShadowCaster" }
+		//	CGPROGRAM
+		//	#pragma vertex VSMain
+		//	#pragma fragment PSMain
+
+		//	float4 VSMain(float4 vertex:POSITION) : SV_POSITION
+		//	{
+		//		return UnityObjectToClipPos(vertex);
+		//	}
+
+		//	float4 PSMain(float4 vertex:SV_POSITION) : SV_TARGET
+		//	{
+		//		return 0;
+		//	}
+
+		//	ENDCG
+		//}
+	}
+}
---
+++ Freedom/Assets/Test6_Reflection/Test6_Reflection.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+
+public class Test6_Reflection : MonoBehaviour
+{
+    private Camera reflectionCamera = null;
+    private RenderTexture reflectionRT = null;
+    private bool isReflectionCameraRendering = false;
+    private Material reflectionMaterial = null;
+
+    private void OnWillRenderObject()
+    {
+        if (isReflectionCameraRendering)
+            return;
+
+        isReflectionCameraRendering = true;
+
+        if (reflectionCamera == null)
+        {
+            var go = new GameObject("Reflection Camera");
+            reflectionCamera = go.AddComponent<Camera>();
+            reflectionCamera.CopyFrom(Camera.current);
+        }
+        else
+        {
+            reflectionCamera.CopyFrom(Camera.current);
+        }
+        if (reflectionRT == null)
+        {
+            reflectionRT = RenderTexture.GetTemporary(1024, 1024, 24);
+        }
+        //需要实时同步相机的参数，比如编辑器下滚动滚轮，Editor相机的远近裁剪面就会变化
+        UpdateCamearaParams(Camera.current, reflectionCamera);
+        reflectionCamera.targetTexture = reflectionRT;
+        reflectionCamera.enabled = false;
+
+        var reflectM = CaculateReflectMatrix();
+        reflectionCamera.worldToCameraMatrix = Camera.current.worldToCameraMatrix * reflectM;
+
+        var normal = transform.up;
+        var d = -Vector3.Dot(normal, transform.position);
+        var plane = new Vector4(normal.x, normal.y, normal.z, d);
+        //用逆转置矩阵将平面从世界空间变换到反射相机空间
+        var viewSpacePlane = reflectionCamera.worldToCameraMatrix.inverse.transpose * plane;
+        var clipMatrix = reflectionCamera.CalculateObliqueMatrix(viewSpacePlane);
+        reflectionCamera.projectionMatrix = clipMatrix;
+
+        GL.invertCulling = true;
+        reflectionCamera.Render();
+        GL.invertCulling = false;
+
+        if (reflectionMaterial == null)
+        {
+            var renderer = GetComponent<Renderer>();
+            reflectionMaterial = renderer.sharedMaterial;
+        }
+        reflectionMaterial.SetTexture("_ReflectionTex", reflectionRT);
+
+        isReflectionCameraRendering = false;
+    }
+
+    Matrix4x4 CaculateReflectMatrix()
+    {
+        var normal = transform.up;
+        var d = -Vector3.Dot(normal, transform.position);
+        var reflectM = new Matrix4x4();
+        reflectM.m00 = 1 - 2 * normal.x * normal.x;
+        reflectM.m01 = -2 * normal.x * normal.y;
+        reflectM.m02 = -2 * normal.x * normal.z;
+        reflectM.m03 = -2 * d * normal.x;
+
+        reflectM.m10 = -2 * normal.x * normal.y;
+        reflectM.m11 = 1 - 2 * normal.y * normal.y;
+        reflectM.m12 = -2 * normal.y * normal.z;
+        reflectM.m13 = -2 * d * normal.y;
+
+        reflectM.m20 = -2 * normal.x * normal.z;
+        reflectM.m21 = -2 * normal.y * normal.z;
+        reflectM.m22 = 1 - 2 * normal.z * normal.z;
+        reflectM.m23 = -2 * d * normal.z;
+
+        reflectM.m30 = 0;
+        reflectM.m31 = 0;
+        reflectM.m32 = 0;
+        reflectM.m33 = 1;
+        return reflectM;
+    }
+
+    private void UpdateCamearaParams(Camera srcCamera, Camera destCamera)
+    {
+        if (destCamera == null || srcCamera == null)
+            return;
+
+        destCamera.clearFlags = srcCamera.clearFlags;
+        destCamera.backgroundColor = srcCamera.backgroundColor;
+        destCamera.farClipPlane = srcCamera.farClipPlane;
+        destCamera.nearClipPlane = srcCamera.nearClipPlane;
+        destCamera.orthographic = srcCamera.orthographic;
+        destCamera.fieldOfView = srcCamera.fieldOfView;
+        destCamera.aspect = srcCamera.aspect;
+        destCamera.orthographicSize = srcCamera.orthographicSize;
+    }
+
+    private Matrix4x4 CaculateObliqueViewFrustumMatrix(Vector4 plane, Camera camera)
+    {
+        var viewSpacePlane = camera.worldToCameraMatrix.inverse.transpose * plane;
+        return camera.CalculateObliqueMatrix(viewSpacePlane);
+    }
+}
